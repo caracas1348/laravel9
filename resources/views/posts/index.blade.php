a36f9aa0d1482b567fc7cc6bc62d7209e9fd1b64
caracas1348 <caracas1348@gmail.com>
2023-09-11 14:53:31 -0400

    commit-02
    ajuste error de arranque

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,7 @@
 {{-- @dump($posts) --}}
 @foreach ($posts as $post)
 {{-- <h1>  {{ $post->title; }}  </h1> --}}
-   <h2><a href="{{ route('posts.show', $post->id;)}}">{{$post->title;}}</a></h2>
+   <h2><a href="{{ route('posts.show', $post->id);}}">{{$post->title;}}</a></h2>
 @endforeach
 @endsection
 
