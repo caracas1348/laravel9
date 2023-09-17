8c9422210848de941cbd7effd5dea62c10abf9ff
caracas1348 <caracas1348@gmail.com>
2023-09-17 11:28:42 -0400

    leccion 23
    asignacion masiva

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','body'];
+    //protected $fillable = ['title','body'];
+    protected $guarded = [];
+              //$guarded //son los campos que no se pueden asignar de forma masiva o sea si se deja vacio deja pasar todos los campos
     //protected $table = 'posts';// por defecto toma el nombre de la clase en plural, pero si quieres que tenga otro nombre cambiar el valor del atributo
 }
